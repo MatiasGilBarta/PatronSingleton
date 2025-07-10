142f72971cfd3a7af5a240ba2c5f972837820ef3
MatiasGilBarta <matiasgilbarta.dev@gmail.com>
2025-07-09 23:19:44 -0300

    patron singleton

---
+++ Domain/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public sealed class Logger
+    {   
+        private static readonly Logger _instance = new Logger();
+
+        public static Logger Current
+        { 
+            get 
+            {
+                return _instance;
+            } 
+        }
+        private Logger() 
+        {
+            //implementacion del singleton
+        }
+
+        public void Guardar(string mensaje)
+        { Console.WriteLine("guardando mensaje: " + mensaje); }
+    }
+}
---
+++ UI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "lo que hay que guardar";
+
+            Logger.Current.Guardar(a);
+            Console.ReadLine();
+        }
+
+    }
+}
